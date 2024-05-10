5b62c6e815cd1217da5522ae7de336fdaa085cd8
markmdoza <marklmdoza@gmail.com>
2024-05-10 10:21:17 -0400

    Initial setup of xcode

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Mark Mendoza on 2024-05-10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
